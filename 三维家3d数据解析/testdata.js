dd3c7cf5a13c7cb044a19cdd3e4a213f340dd438
huaweicong <416817604@qq.com>
2024-04-12 15:22:28 +0800

    update

---
+++ 三维家3d数据解析/testdata.js
@@ -0,0 +1,341 @@
+var list = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "2",
+        EndX: "491.50",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "3",
+        EndX: "491.50",
+        EndY: "472.50",
+        Angle: "0",
+    },
+    {
+        LineID: "4",
+        EndX: "691.50",
+        EndY: "672.50",
+        Angle: "-90",
+    },
+    {
+        LineID: "5",
+        EndX: "1164.00",
+        EndY: "672.50",
+        Angle: "0",
+    },
+    {
+        LineID: "6",
+        EndX: "1164.00",
+        EndY: "1164.00",
+        Angle: "0",
+    },
+    {
+        LineID: "7",
+        EndX: "0.00",
+        EndY: "1164.00",
+        Angle: "0",
+    },
+];
+
+var list2 = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "474.00",
+        Angle: "90",
+    },
+    {
+        LineID: "2",
+        EndX: "0.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "3",
+        EndX: "18.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "4",
+        EndX: "18.00",
+        EndY: "474.00",
+        Angle: "0",
+    },
+    {
+        LineID: "5",
+        EndX: "218.00",
+        EndY: "674.00",
+        Angle: "-90",
+    },
+    {
+        LineID: "6",
+        EndX: "691.00",
+        EndY: "674.00",
+        Angle: "0",
+    },
+    {
+        LineID: "7",
+        EndX: "691.00",
+        EndY: "692.00",
+        Angle: "0",
+    },
+    {
+        LineID: "8",
+        EndX: "218.00",
+        EndY: "692.00",
+        Angle: "0",
+    },
+];
+
+const list3 = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "150.00",
+        Angle: "0",
+    },
+    {
+        LineID: "2",
+        EndX: "150.00",
+        EndY: "0.00",
+        Angle: "90",
+    },
+    {
+        LineID: "3",
+        EndX: "331.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "4",
+        EndX: "331.00",
+        EndY: "381.00",
+        Angle: "0",
+    },
+    {
+        LineID: "5",
+        EndX: "0.00",
+        EndY: "381.00",
+        Angle: "0",
+    },
+];
+
+const list4 = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "150.00",
+        Angle: "0",
+    },
+    {
+        LineID: "2",
+        EndX: "150.00",
+        EndY: "0.00",
+        Angle: "90",
+    },
+    {
+        LineID: "3",
+        EndX: "330.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "4",
+        EndX: "330.00",
+        EndY: "18.00",
+        Angle: "0",
+    },
+    {
+        LineID: "5",
+        EndX: "150.00",
+        EndY: "18.00",
+        Angle: "0",
+    },
+    {
+        LineID: "6",
+        EndX: "18.00",
+        EndY: "150.00",
+        Angle: "-90",
+    },
+    {
+        LineID: "7",
+        EndX: "18.00",
+        EndY: "380.00",
+        Angle: "0",
+    },
+    {
+        LineID: "8",
+        EndX: "0.00",
+        EndY: "380.00",
+        Angle: "0",
+    },
+];
+
+const list5 = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "150.00",
+        Angle: "0",
+    },
+    {
+        LineID: "2",
+        EndX: "150.00",
+        EndY: "0.00",
+        Angle: "90",
+    },
+    {
+        LineID: "3",
+        EndX: "350.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "4",
+        EndX: "350.00",
+        EndY: "400.00",
+        Angle: "0",
+    },
+    {
+        LineID: "5",
+        EndX: "0.00",
+        EndY: "400.00",
+        Angle: "0",
+    },
+];
+
+const list6 = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "2",
+        EndX: "700.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "3",
+        EndX: "700.00",
+        EndY: "450.00",
+        Angle: "-180",
+    },
+    {
+        LineID: "4",
+        EndX: "0.00",
+        EndY: "450.00",
+        Angle: "0",
+    },
+];
+
+const list7 = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "2",
+        EndX: "475.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "3",
+        EndX: "700.00",
+        EndY: "225.00",
+        Angle: "90",
+    },
+    {
+        LineID: "4",
+        EndX: "475.00",
+        EndY: "450.00",
+        Angle: "90",
+    },
+    {
+        LineID: "5",
+        EndX: "0.00",
+        EndY: "450.00",
+        Angle: "0",
+    },
+];
+
+
+
+const list8 = [
+    {
+        LineID: "1",
+        EndX: "0.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "2",
+        EndX: "400.00",
+        EndY: "0.00",
+        Angle: "0",
+    },
+    {
+        LineID: "3",
+        EndX: "400.00",
+        EndY: "250.00",
+        Angle: "0",
+    },
+    {
+        LineID: "4",
+        EndX: "250.00",
+        EndY: "400.00",
+        Angle: "90",
+    },
+    {
+        LineID: "5",
+        EndX: "0.00",
+        EndY: "400.00",
+        Angle: "0",
+    },
+];
+
+const list10 = [
+    {
+        "LineID": "1",
+        "EndX": "0.00",
+        "EndY": "150.00",
+        "Angle": "0"
+    },
+    {
+        "LineID": "2",
+        "EndX": "150.00",
+        "EndY": "0.00",
+        "Angle": "90"
+    },
+    {
+        "LineID": "3",
+        "EndX": "331.00",
+        "EndY": "0.00",
+        "Angle": "0"
+    },
+    {
+        "LineID": "4",
+        "EndX": "331.00",
+        "EndY": "381.00",
+        "Angle": "0"
+    },
+    {
+        "LineID": "5",
+        "EndX": "0.00",
+        "EndY": "381.00",
+        "Angle": "0"
+    }
+]
+
